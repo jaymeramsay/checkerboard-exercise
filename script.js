d68ec440ecb479b48d5fd887fdda1cb133442494
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-08-26 14:00:56 -0500

    checkboard now has red and black tiles

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// Your JS goes here
+window.onload = function () {
+  myCode();
+}
+
+function myCode() {
+  console.log('booyah');
+}
+//create element
+let square;
+let container;
+for (let i = 0; i < 63; i++) {
+  // make it look like a square
+  square = document.createElement('div');
+  square.addEventListener('click', function (ev) {
+    console.log(ev.target);
+    console.log('HEY I WAS CLICKED');
+    console.log(square);
+
+  })
+  if (i % 2 === 0) {
+    decorateSquare(square, "tomato");
+  }
+  else {
+    decorateSquare(square, "black");
+  }
+
+  container = document.getElementsByTagName('body')[0];
+  //add it to the DOM
+  //find something to append to
+  //append to that thing
+  container.append(square);
+}
+
+function decorateSquare(sq, color) {
+  sq.style.width = "11.1%";
+  sq.style.paddingBottom = "11.1%";
+  sq.style.backgroundColor = color;
+  // sq.style.border = "1px solid white";
+  sq.style.float = "left";
+}
+
+
+
+console.log(square);
