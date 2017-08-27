0c78936c4fbbc64dffa11d5efba21916903c15d7
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-08-26 20:05:22 -0500

    refactored checkerboard for gradient style

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-window.onload = function () {
-  myCode();
-}
+window.onload = function () {}
 
 //create container element to hold all of my squares
 let container = document.getElementsByTagName('body')[0];
@@ -15,23 +13,11 @@ for (let i = 0; i < 63; i++) {
   decorateSquare(square);
 }
 
-//create function to decorate square colors randomly
-function randomColors(sq) {
-  //I want each square's background color to set to a random color
-  //loop through the squares and assign color
-  for (let i = 0; i < 63; i++) {
-    //create a variable to hold the randomly changing colors. RGB.
-    let hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-    //set backgroundColor to variable
-    sq.style.backgroundColor = hue;
-  }
-}
-
 //create function to actually decorate the squares in JS. NOT CSS!
 function decorateSquare(sq, color) {
   sq.style.width = "11.1%";
   sq.style.paddingBottom = "11.1%";
   sq.style.float = "left";
   sq.style.display = "inlineblock";
-  sq.style.backgroundColor = randomColors(sq);
+  sq.style.background = 'linear-gradient(45deg, red, blue)'
 }
